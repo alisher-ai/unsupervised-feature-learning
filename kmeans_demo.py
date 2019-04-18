8223859589c2c99035912f0dda52a9b035e7a140
Alisher <alisher@browzzin.com>
2019-04-18 21:13:06 +0900

    first running version

--- kmeans_demo.py
+++ kmeans_demo.py
@@ -1,16 +1,18 @@
-import os
-
+from extract_features import extract_features_from_data, extract_features_from_whitened_data, \
+    extract_features_post_processing
 import numpy as np
+from sklearn import svm
 
-from utils import load_cifar10_training_data, extract_random_patches, normalize_for_contrast, data_whitening
+from utils import load_cifar10_training_data, extract_random_patches, normalize_for_contrast, data_whitening, \
+    load_cifar10_test_data
 from run_kmeans import run_kmeans
 from plot_centroids import plot_centroids
 
 dataset_dir = "/ext/alisher_research/cifar-10-batches-py"
 
 rf_size = 6
-num_centroids = 1600
-whitening = True
+num_centroids = 400
+whitening = False
 num_patches = 40000
 image_dimensions = [32, 32, 3]
 
@@ -18,64 +20,31 @@ train_x, train_y = load_cifar10_training_data(dataset_dir)
 patches = extract_random_patches(num_patches, rf_size, train_x, train_y, image_dimensions)
 patches = normalize_for_contrast(patches)
 if whitening:
-    patches = data_whitening(patches)
+    patches, m, p = data_whitening(patches)
 
 
-""" run k-means
-"""
+""" run k-means """
 centroids = run_kmeans(patches, num_centroids, 50)
-# plot_centroids(centroids, rf_size, savefile)
+plot_centroids(centroids=centroids, width=rf_size, height=rf_size)
 
+""" extract training features """
+trainXCs = extract_features_post_processing(whitening, train_x, centroids, rf_size, image_dimensions)
 
-"""
+""" Support Vector Machine (SVM) Classifier training """
+clf = svm.SVC(kernel='rbf', verbose=True, max_iter=1000000, C=100)
+clf.fit(trainXCs, train_y)
+print(clf.score(trainXCs, train_y))
 
+""" Testing the model on test data: """
+test_x, test_y = load_cifar10_test_data(dataset_dir)
 
-% run K-means
-centroids = run_kmeans(patches, numCentroids, 50);
-show_centroids(centroids, rfSize); drawnow;
+if whitening:
+    testXC = extract_features_from_whitened_data(test_x, centroids, rf_size, image_dimensions, m, p)
+else:
+    testXC = extract_features_from_data(test_y, centroids, rf_size, image_dimensions)
 
-% extract training features
-if (whitening)
-  trainXC = extract_features(trainX, centroids, rfSize, CIFAR_DIM, M,P);
-else
-  trainXC = extract_features(trainX, centroids, rfSize, CIFAR_DIM);
-end
+testXCs = extract_features_post_processing(whitening, test_x, centroids, rf_size, image_dimensions)
 
-% standardize data
-trainXC_mean = mean(trainXC);
-trainXC_sd = sqrt(var(trainXC)+0.01);
-trainXCs = bsxfun(@rdivide, bsxfun(@minus, trainXC, trainXC_mean), trainXC_sd);
-trainXCs = [trainXCs, ones(size(trainXCs,1),1)];
-
-% train classifier using SVM
-C = 100;
-theta = train_svm(trainXCs, trainY, C);
-
-[val,labels] = max(trainXCs*theta, [], 2);
-fprintf('Train accuracy %f%%\n', 100 * (1 - sum(labels ~= trainY) / length(trainY)));
-
-%%%%% TESTING %%%%%
-
-%% Load CIFAR test data
-fprintf('Loading test data...\n');
-f1=load([CIFAR_DIR '/test_batch.mat']);
-testX = double(f1.data);
-testY = double(f1.labels) + 1;
-clear f1;
-
-% compute testing features and standardize
-if (whitening)
-  testXC = extract_features(testX, centroids, rfSize, CIFAR_DIM, M,P);
-else
-  testXC = extract_features(testX, centroids, rfSize, CIFAR_DIM);
-end
-testXCs = bsxfun(@rdivide, bsxfun(@minus, testXC, trainXC_mean), trainXC_sd);
-testXCs = [testXCs, ones(size(testXCs,1),1)];
-
-% test and print result
-[val,labels] = max(testXCs*theta, [], 2);
-fprintf('Test accuracy %f%%\n', 100 * (1 - sum(labels ~= testY) / length(testY)));
-
-
-
-"""
+testXCs = testXCs[:10, :]
+y_pred = clf.predict(testXCs)
+print(clf.score(testXCs, test_y))
