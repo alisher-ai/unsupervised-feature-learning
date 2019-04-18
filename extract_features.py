a204d051c8cb958fe6110ef685113d6908bf0897
Alisher <alisher@browzzin.com>
2019-04-18 21:12:54 +0900

    extract features from images

--- extract_features.py
+++ extract_features.py
@@ -1,84 +1,77 @@
 import numpy as np
+from utils import array2im, im2col_sliding_strided, normalize_for_contrast
 
 
-def extract_features(**kwargs):
-    if not len(kwargs.items()) == 4 or not len(kwargs.items()) == 6:
-        return None
+def extract_features_from_whitened_data(data_x, centroids, rf_size, image_dimensions, m, p):
+    return None
 
-    data_x = kwargs.get("X", None)
-    centroids = kwargs.get("centroids", None)
-    rf_size = kwargs.get("rfSize", None)
-    cifar_dim = kwargs.get("CIFAR_DIM", None)
-    m = kwargs.get("M", None)
-    p = kwargs.get("P", None)
 
-    if True in (f for f in [data_x, centroids, rf_size, cifar_dim, m, p]):
-        whitening = True
-    else:
-        return None
-
+def extract_features_from_data(data_x, centroids, rf_size, image_dimensions):
     num_centroids = centroids.shape[0]
 
-    data_x_centroids = np.zeros(data_x.shape[0], num_centroids * 4)
+    data_x_centroids = np.zeros((data_x.shape[0], num_centroids * 4))
 
     for i, x in enumerate(data_x):
         if i % 1000 == 0:
                 print("Extracting features: {} / {}".format(i, data_x.shape[0]))
-        patches = []
+        patches = np.transpose(np.concatenate((im2col_sliding_strided(data_x[i, :1024].reshape(32, 32), window=[6, 6]),
+                                               im2col_sliding_strided(data_x[i, 1024:2048].reshape(32, 32), window=[6, 6]),
+                                               im2col_sliding_strided(data_x[i, 2048:].reshape(32, 32), window=[6, 6])), axis=0))
+
+        """ normalize for contrast
+        """
+        patches = np.transpose(normalize_for_contrast(patches))
+
+        """ compute 'triangle' activation function
+        """
+        xx = np.sum(np.power(patches, 2), axis=1)
+        cc = np.sum(np.power(centroids, 2), axis=1)
+        xc = np.matmul(patches, np.transpose(centroids))
+
+        z1 = np.reshape(xx, (-1, 1)) - 2 * xc
+        z2 = z1 + np.reshape(cc, (1, -1))
+        z = np.sqrt(z2)
+
+        val, inds = [z.min(1), z.argmin(1)]
+
+        """ average distance to centroids for each patch
+        """
+        mu = np.mean(z, 1)
+
+        """ patches is now the data matrix of activations for each patch
+        """
+        patches = np.reshape(mu, (-1, 1)) - z
+        patches[np.where(patches < 0)] = 0
+
+        prows = image_dimensions[0] - rf_size + 1
+        pcols = image_dimensions[1] - rf_size + 1
+
+        patches = patches.reshape((prows, pcols, num_centroids))
+        halfr = int(round(float(prows) / 2))
+        halfc = int(round(float(pcols) / 2))
+
+        q1 = sum(sum(patches[:halfr, :halfc, :]))
+        q2 = sum(sum(patches[halfr:, :halfc, :]))
+        q3 = sum(sum(patches[:halfr, halfc:, :]))
+        q4 = sum(sum(patches[halfr:, halfc:, :]))
+
+        data_x_centroids[i, :] = np.concatenate((q1, q2, q3, q4), axis=0)
+
+    return data_x_centroids
 
 
+def extract_features_post_processing(whitening, data_x, centroids, rf_size, image_dimensions, m=None, p=None):
+    if whitening:
+        trainXC = extract_features_from_whitened_data(data_x, centroids, rf_size, image_dimensions, m, p)
+    else:
+        trainXC = extract_features_from_data(data_x, centroids, rf_size, image_dimensions)
 
+    trainXC_mean = np.mean(trainXC, 0)
+    trainXC_sd = np.sqrt(np.var(trainXC, 0, ddof=1) + 0.01)
 
-"""
+    trainXCs = np.transpose((trainXC - trainXC_mean) / trainXC_sd)
+    trainXCs = np.concatenate((np.transpose(trainXCs), np.ones((trainXCs.shape[1], 1))), axis=1)
 
-    
-function XC = extract_features(X, centroids, rfSize, CIFAR_DIM, M,P)
-  
-  % compute features for all training images
-  XC = zeros(size(X,1), numCentroids*4);
-  for i=1:size(X,1)
-    
-    % extract overlapping sub-patches into rows of 'patches'
-    patches = [ im2col(reshape(X(i,1:1024),CIFAR_DIM(1:2)), [rfSize rfSize]) ;
-                im2col(reshape(X(i,1025:2048),CIFAR_DIM(1:2)), [rfSize rfSize]) ;
-                im2col(reshape(X(i,2049:end),CIFAR_DIM(1:2)), [rfSize rfSize]) ]';
+    # trainXCs = trainXCs[:10, :]
 
-    % do preprocessing for each patch
-    
-    % normalize for contrast
-    patches = bsxfun(@rdivide, bsxfun(@minus, patches, mean(patches,2)), sqrt(var(patches,[],2)+10));
-    % whiten
-    if (whitening)
-      patches = bsxfun(@minus, patches, M) * P;
-    end
-    
-    % compute 'triangle' activation function
-    xx = sum(patches.^2, 2);
-    cc = sum(centroids.^2, 2)';
-    xc = patches * centroids';
-    
-    z = sqrt( bsxfun(@plus, cc, bsxfun(@minus, xx, 2*xc)) ); % distances
-    [v,inds] = min(z,[],2);
-    mu = mean(z, 2); % average distance to centroids for each patch
-    patches = max(bsxfun(@minus, mu, z), 0);
-    % patches is now the data matrix of activations for each patch
-    
-    % reshape to numCentroids-channel image
-    prows = CIFAR_DIM(1)-rfSize+1;
-    pcols = CIFAR_DIM(2)-rfSize+1;
-    patches = reshape(patches, prows, pcols, numCentroids);
-    
-    % pool over quadrants
-    halfr = round(prows/2);
-    halfc = round(pcols/2);
-    q1 = sum(sum(patches(1:halfr, 1:halfc, :), 1),2);
-    q2 = sum(sum(patches(halfr+1:end, 1:halfc, :), 1),2);
-    q3 = sum(sum(patches(1:halfr, halfc+1:end, :), 1),2);
-    q4 = sum(sum(patches(halfr+1:end, halfc+1:end, :), 1),2);
-    
-    % concatenate into feature vector
-    XC(i,:) = [q1(:);q2(:);q3(:);q4(:)]';
-  end
-  
-
-"""
+    return trainXCs
